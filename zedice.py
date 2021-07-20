d58d598e0938836f4c988eea6fd526f6cc1127f5
Chivalrybean <jlowmaster@gmail.com>
2021-07-19 20:58:13 -0700

    Added dice handling classes.

--- zedice.py
+++ zedice.py
@@ -11,11 +11,34 @@ class Zedie:
     
     def roll(self):
         return self.faces[(rng.randint(0,5))]
+    
+    def __repr__(self) -> str:
+        return "Green Die"
 
 class Red_zedie(Zedie):
     def __init__(self):
         self.faces = [brain, feet, feet, shotgun, shotgun, shotgun]
 
+    def __repr__(self) -> str:
+        return "Red Die"
+
 class Yellow_zedie(Zedie):
     def __init__(self):
         self.faces = [brain, brain, feet, feet, shotgun, shotgun]
+
+    def __repr__(self) -> str:
+        return "Yellow Die"
+
+class Dice_bag:
+    def __init__(self):
+        self.contents = [Zedie, Zedie, Zedie, Zedie, Zedie, Zedie, Yellow_zedie, Yellow_zedie, Yellow_zedie, Yellow_zedie, Red_zedie, Red_zedie, Red_zedie]
+
+    def select_dice(self, amount):
+        draw = []
+        i = 0
+        while i < amount:
+            draw.append(self.contents.pop(rng.randint(0,len(self.contents) - 1)))
+            i += 1
+        return draw
+
+    
