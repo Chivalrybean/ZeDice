48375e3a42ce9d39208389a7a4da90f1c27b6bec
Chivalrybean <jlowmaster@gmail.com>
2021-07-19 22:08:04 -0700

    Added dice bag, better rolled dice, fixed dice bag
    Dice_bag held Zedice now Zedice()

--- zedice.py
+++ zedice.py
@@ -5,6 +5,8 @@ brain = "Brain :brain:"
 feet = "Footsteps :footprints:"
 shotgun = "BLAM! :exploding_head:"
 
+#Game Classes
+
 class Zedie:
     def __init__(self):
         self.faces = [brain, brain, brain, feet, feet, shotgun]
@@ -13,32 +15,44 @@ class Zedie:
         return self.faces[(rng.randint(0,5))]
     
     def __repr__(self) -> str:
-        return "Green Die"
+        return f"Green: {self.roll()}"
 
 class Red_zedie(Zedie):
     def __init__(self):
         self.faces = [brain, feet, feet, shotgun, shotgun, shotgun]
 
     def __repr__(self) -> str:
-        return "Red Die"
+        return f"Red: {self.roll()}"
 
 class Yellow_zedie(Zedie):
     def __init__(self):
         self.faces = [brain, brain, feet, feet, shotgun, shotgun]
 
     def __repr__(self) -> str:
-        return "Yellow Die"
+        return f"Yellow: {self.roll()}"
 
 class Dice_bag:
     def __init__(self):
-        self.contents = [Zedie, Zedie, Zedie, Zedie, Zedie, Zedie, Yellow_zedie, Yellow_zedie, Yellow_zedie, Yellow_zedie, Red_zedie, Red_zedie, Red_zedie]
+        self.contents = [Zedie(), Zedie(), Zedie(), Zedie(), Zedie(), Zedie(), Yellow_zedie(), Yellow_zedie(), Yellow_zedie(),
+         Yellow_zedie(), Red_zedie(), Red_zedie(), Red_zedie()]
 
     def select_dice(self, amount):
         draw = []
         i = 0
         while i < amount:
-            draw.append(self.contents.pop(rng.randint(0,len(self.contents) - 1)))
+            draw.append(self.contents.pop(rng.randint(0,len(self.contents) - 1)).roll())
             i += 1
         return draw
 
-    
+class Rolled_dice:
+    def __init__(self, inital_roll) -> None:
+        self.dice_pool = inital_roll
+        self.feet_total = 0
+        self.brain_total = 0
+        self. blam_total = 0
+
+#Discord classes
+
+class Active_game:
+    def __init__(self, server_id, channel_id, players = []) -> None:
+        pass
