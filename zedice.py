cd2ba932d9be2d534ab7d0ffdfe14f950ee3d6c4
Chivalrybean <jlowmaster@gmail.com>
2021-08-06 22:28:39 -0700

    Added comments.

--- zedice.py
+++ zedice.py
@@ -1,12 +1,14 @@
 import random as rng
 import discord
 
+# "Constants" for the output in Discord.
 brain = "Brain :brain:"
 feet = "Footsteps :footprints:"
 shotgun = "BLAM! :exploding_head:"
 
 #Game Classes
 
+#A standard green die, six-sided, weighted with brains, you can roll it and get a random side and it's icon.
 class Zedie:
     def __init__(self):
         self.faces = [brain, brain, brain, feet, feet, shotgun]
@@ -17,6 +19,7 @@ class Zedie:
     def __repr__(self) -> str:
         return f"Green: {self.roll()}"
 
+#Red die, weighted with shotguns (more dangerous)
 class Red_zedie(Zedie):
     def __init__(self):
         self.faces = [brain, feet, feet, shotgun, shotgun, shotgun]
@@ -24,6 +27,7 @@ class Red_zedie(Zedie):
     def __repr__(self) -> str:
         return f"Red: {self.roll()}"
 
+#Yellow die, equal sides.
 class Yellow_zedie(Zedie):
     def __init__(self):
         self.faces = [brain, brain, feet, feet, shotgun, shotgun]
@@ -31,6 +35,8 @@ class Yellow_zedie(Zedie):
     def __repr__(self) -> str:
         return f"Yellow: {self.roll()}"
 
+#The game has 13 dice. You can draw any amount of dice, but typically it's 1 to 3. When a die is rolled, it's removed from the bag.
+#Each active game needs it's own dice bag.
 class Dice_bag:
     def __init__(self):
         self.contents = [Zedie(), Zedie(), Zedie(), Zedie(), Zedie(), Zedie(), Yellow_zedie(), Yellow_zedie(), Yellow_zedie(),
@@ -44,6 +50,7 @@ class Dice_bag:
             i += 1
         return draw
 
+#Which dice have been rolled for an active game. The initial roll is 3 dice from the dice bag.
 class Rolled_dice:
     def __init__(self, inital_roll) -> None:
         self.dice_pool = inital_roll
@@ -53,6 +60,9 @@ class Rolled_dice:
 
 #Discord classes
 
+#Any given server can have one active game in any given channel. If a non-player or player who'd turn it isn't, tries to run a command,
+#it will check versus current player and return an error. 
+#Will need to allow admins or perhaps a time out so a stalled game doesn't prevent play forever.
 class Active_game:
     def __init__(self, server_id, channel_id, players = []) -> None:
         self.location = (server_id, channel_id)
